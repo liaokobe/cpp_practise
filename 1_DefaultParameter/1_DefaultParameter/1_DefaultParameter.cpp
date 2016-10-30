20c9dc00252aaad13cae28f2258a889a40885ac9
liaokobe <5803104@qq.com>
2016-10-30 12:12:54 +0800

    Default parameter and inheritance.
    This is a demo for default parameter and inheritance.

---
+++ 1_DefaultParameter/1_DefaultParameter/1_DefaultParameter.cpp
@@ -0,0 +1,37 @@
+// 1_DefaultParameter.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include<iostream>
+using namespace std;
+
+class A  
+{  
+public:  
+    virtual void Fun(int number = 10)  
+    {  
+        std::cout << "A::Fun with number " << number << endl;  
+    }  
+};  
+
+class B:public A  
+{  
+public:  
+    virtual void Fun(int number = 20)  
+    {  
+        std::cout << "B::Fun with number " << number << endl;  
+    }  
+};  
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    B b;  
+    A &a = b;  
+    A *aa = new B;  
+    aa->Fun();  
+    a.Fun();
+
+    system("pause");
+}
+
