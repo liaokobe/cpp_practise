8b641a27abb6472836b11cc57f1c1a25232742e1
liaokobe <5803104@qq.com>
2016-10-30 12:19:18 +0800

    Formatting.
    Formatting.

--- 1_DefaultParameter/1_DefaultParameter/1_DefaultParameter.cpp
+++ 1_DefaultParameter/1_DefaultParameter/1_DefaultParameter.cpp
@@ -6,32 +6,31 @@
 #include<iostream>
 using namespace std;
 
-class A  
-{  
-public:  
-    virtual void Fun(int number = 10)  
-    {  
-        std::cout << "A::Fun with number " << number << endl;  
-    }  
-};  
+class A
+{
+public:
+    virtual void Fun(int number = 10)
+    {
+        std::cout << "A::Fun with number " << number << endl;
+    }
+};
 
-class B:public A  
-{  
-public:  
-    virtual void Fun(int number = 20)  
-    {  
-        std::cout << "B::Fun with number " << number << endl;  
-    }  
-};  
+class B:public A
+{
+public:
+    virtual void Fun(int number = 20)
+    {
+        std::cout << "B::Fun with number " << number << endl;
+    }
+};
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-    B b;  
-    A &a = b;  
-    A *aa = new B;  
-    aa->Fun();  
+    B b;
+    A &a = b;
+    A *aa = new B;
+    aa->Fun();
     a.Fun();
 
     system("pause");
 }
-
